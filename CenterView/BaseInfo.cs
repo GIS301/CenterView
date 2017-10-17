0f2a9cb8551aa1d71276475608a7251466355e2e
GIS02 <2177687110@qq.com>
2017-10-17 13:25:52 +0800

    去除BaseInfo的授信站点

--- CenterView/BaseInfo.cs
+++ CenterView/BaseInfo.cs
@@ -200,29 +200,6 @@ namespace CenterView
             }
 
         }
-        /// <summary>
-        /// 获取授信站点列表
-        /// </summary>
-        /// <returns></returns>
-        public string[] GetTrustyStations()
-        {
-            string[] subkeyNames;
-            RegistryKey hkml = Registry.CurrentUser;
-            RegistryKey software = hkml.OpenSubKey("SOFTWARE", true);
-            RegistryKey aimdir = software.OpenSubKey(@"Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains", true);
-            subkeyNames = aimdir.GetSubKeyNames();
-            //判断有没有域名前缀例如“www”
-            for (int i = 0; i < subkeyNames.Length; i++)
-            {
-                RegistryKey temp = aimdir.OpenSubKey(subkeyNames[i], false);
-                if (temp.GetSubKeyNames().Length == 1)
-                {
-                    subkeyNames[i] = temp.GetSubKeyNames()[0] + "." + subkeyNames[i];
-                }
-
-            }
-            hkml.Close();
-            return subkeyNames;
-        }
+     
     }
 }
