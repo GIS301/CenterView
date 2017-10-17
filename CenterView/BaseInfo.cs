51e8919436311b7d71509a0550970dbf4f5e9142
GuoHongSheng <1490939331@qq.com>
2017-10-17 22:17:37 +0800

    BaseInfo补�

--- CenterView/BaseInfo.cs
+++ CenterView/BaseInfo.cs
@@ -17,7 +17,7 @@ namespace CenterView
 
             // 硬件
             Win32_Processor,     // CPU 处理器
-            Win32_PhysicalMemory,  //物理内存条
+            Win32_PhysicalMemory,  // 物理内存条
             Win32_Keyboard,     // 键盘
             Win32_PointingDevice,  // 点输入设备，包括鼠标。
             Win32_FloppyDrive,    // 软盘驱动器
@@ -69,80 +69,184 @@ namespace CenterView
             #endregion
         }
 
+
+
         /// <summary>
-        /// 构造函数
+        /// 电脑型号
         /// </summary>
-        public BaseInfo()
+        /*  public string GetVersions()
+          {
+              var result = "";
+              try
+              {
+
+                  ManagementClass hardDisk = new ManagementClass("Win32_ComputerSystemProduct");
+                  ManagementObjectCollection hardDiskC = hardDisk.GetInstances();
+                  foreach (ManagementObject m in hardDiskC)
+                  {
+
+                      result += m["ComputerSystemProduct"].ToString();
+                      break;
+                  }
+              }
+              catch
+              {
+
+              }
+              return result;
+          }*/
+        ///获取电脑制造商信息
+        public string GetVersions()
         {
+            var result = "";
+            // create management class object
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            //collection to store all management objects
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    result += string.Format("\n {0}",
+                                      mo["Manufacturer"].ToString());
+                }
+            }
+            //wait for user action
+            return result;
         }
         /// <summary>
-        /// 获取所有硬件信息
+        /// 获取计算机类型
         /// </summary>
-        public void GetAllBaseInfos()
+        public enum ChassisTypes
         {
-            hinfos.OSystem = GetOSystemType();
+            Other = 1,
+            Unknown,
+            Desktop,
+            LowProfileDesktop,
+            PizzaBox,
+            MiniTower,
+            Tower,
+            Portable,
+            Laptop,
+            Notebook,
+            Handheld,
+            DockingStation,
+            AllInOne,
+            SubNotebook,
+            SpaceSaving,
+            LunchBox,
+            MainSystemChassis,
+            ExpansionChassis,
+            SubChassis,
+            BusExpansionChassis,
+            PeripheralChassis,
+            StorageChassis,
+            RackMountChassis,
+            SealedCasePC
         }
+
+        public string GetChassisTypes()
+        {
+            var result = "";
+            ManagementClass systemEnclosures = new ManagementClass("Win32_SystemEnclosure");
+            foreach (ManagementObject obj in systemEnclosures.GetInstances())
+            {
+                foreach (int i in (UInt16[])(obj["ChassisTypes"]))
+                {
+                    if (i > 0 && i < 25)
+                    {
+                        return ((ChassisTypes)i).ToString();
+                    }
+                }
+            }
+            return ChassisTypes.Unknown.ToString();
+        }
+        /// <summary>
+        /// 获取计算机名
+        /// </summary>
+        /// <returns></returns>
+        public string GetMachineName()
+        {
+            try
+            {
+                return System.Environment.MachineName;
+            }
+            catch (Exception e)
+            {
+                return "uMnNk";
+            }
+        }
+
         /// <summary>
         /// 检测操作系统信息
         /// </summary>
-        public ManagementObjectCollection OsInfo()
+        public string GetOsInfo()
         {
-
+            string result = "";
             try
             {
                 ManagementClass mc = new ManagementClass(WMIPath.Win32_OperatingSystem.ToString());
                 ManagementObjectCollection moc = mc.GetInstances();
-                return moc;
+                foreach (var item in moc)
+                {
+
+
+                    var text = item.Properties["Name"].Value + "\r\n";
+                    result += text.Split('|')[0];
+
+                }
+                return result;
             }
             catch
             {
                 return null;
             }
         }
-
-        /// <summary>
-        /// 获取操作系统类型
-        /// </summary>
-        /// <returns></returns>
-        public string GetOSystemType()
+        /// <summary> 
+        /// 获取操作系统位数（x32/64） 
+        /// </summary> 
+        /// <returns>int</returns> 
+        public string GetOSBit()
         {
-            return Environment.OSVersion.ToString();
-        }
-        /// <summary>
-        /// 获取系统类型
-        /// </summary>
-        /// <returns></returns>
-        public string GetSystemType()
-        {
+            string result = "";
             try
             {
-                string st = "";
-                ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
-                ManagementObjectCollection moc = mc.GetInstances();
-                foreach (ManagementObject mo in moc)
+                ConnectionOptions mConnOption = new ConnectionOptions();
+                ManagementScope mMs = new ManagementScope(@"\\localhost", mConnOption);
+                ObjectQuery mQuery = new ObjectQuery("select AddressWidth from Win32_Processor");
+                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher(mMs, mQuery);
+                ManagementObjectCollection mObjectCollection = mSearcher.Get();
+                foreach (ManagementObject mObject in mObjectCollection)
                 {
-                    st = mo["SystemType"].ToString();
+                    result += mObject["AddressWidth"].ToString() + "位" + "\r\n";
                 }
-                moc = null;
-                mc = null;
-                return st;
+                return result;
             }
             catch
             {
-                return "unknow";
+
+                return null;
             }
         }
+
         /// <summary>
         /// 检测Cpu信息
         /// </summary>
         /// <returns></returns>
-        public ManagementObjectCollection CpuInfo()
+        public string GetCpuInfo()
         {
+            string result = "";
             try
             {
                 ManagementClass mc = new ManagementClass(WMIPath.Win32_Processor.ToString());
                 ManagementObjectCollection moc = mc.GetInstances();
-                return moc;
+                foreach (var item in moc)
+                {
+                    result += item.Properties["Name"].Value + "\r\n";
+
+                }
+                return result;
             }
             catch
             {
@@ -150,55 +254,266 @@ namespace CenterView
             }
 
         }
+        ///检测内存
+
+        public string GetMemoryInfo()
+        {
+            var result = "";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher();   //用于查询一些如系统信息的管理对象 
+            searcher.Query = new SelectQuery("Win32_PhysicalMemory ", "", new string[] { "Capacity" });//设置查询条件 
+            ManagementObjectCollection collection = searcher.Get();   //获取内存容量 
+            ManagementObjectCollection.ManagementObjectEnumerator em = collection.GetEnumerator();
+
+            long capacity = 0;
+            while (em.MoveNext())
+            {
+                ManagementBaseObject baseObj = em.Current;
+                if (baseObj.Properties["Capacity"].Value != null)
+                {
+                    try
+                    {
+                        capacity += long.Parse(baseObj.Properties["Capacity"].Value.ToString());
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+            }
+            var theresult = (int)(capacity / 1024 / 1024);
+            result = theresult.ToString();
+            return result;
+        }
         /// <summary>
         /// 检测硬盘信息
         /// </summary>
-        public ManagementObjectCollection DiskDriveInfo()
+
+
+        public string GetDiskDriveInfo()
         {
+            //string result = "";
+            //System.Management.ManagementClass mc = new System.Management.ManagementClass("Win32_DiskDrive");
+            //System.Management.ManagementObjectCollection moc = mc.GetInstances();
+            //foreach (System.Management.ManagementObject mo in moc)
+            //{
+            //    if (result == "")
+            //    {
+            //        try
+            //        {
+            //            //result += mo["Manufacturer"].ToString()+" ";//制造商
+            //            //result += mo["Signature"].ToString() + " ";//签名
+            //            result += Convert.ToDouble(mo.Properties["Size"].Value) / (1024 * 1024 * 1024);
+
+            //            break;
+            //        }
+            //        catch
+            //        {
+            //        }
+            //    }
+
+            //}
+            //return result;
+
+
+
+
+            var result = "";
+            double Size = 0;
             try
             {
                 ManagementClass mc = new ManagementClass(WMIPath.Win32_DiskDrive.ToString());
-                ManagementObjectCollection moc = mc.GetInstances();
-                return moc;
+                ManagementObjectCollection theDiskDriveInfo = mc.GetInstances();
+                foreach (var item in theDiskDriveInfo)
+                {
+
+
+                    Size += Convert.ToDouble(item.Properties["Size"].Value) / (1024 * 1024 * 1024);
+                }
+                if (Size > 1024)
+                {
+                    result = Math.Round((Size / 1024), 2) + "T";
+                }
+                else
+                {
+                    result = Math.Round(Size, 2) + "G";
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+
+        }
+        /// <summary>
+        /// 显卡 芯片,显存大小
+        /// </summary>
+        public string GetVideoController()
+        {
+            var result = "";
+
+            try
+            {
+
+                ManagementClass hardDisk = new ManagementClass("Win32_VideoController");
+                ManagementObjectCollection hardDiskC = hardDisk.GetInstances();
+                foreach (ManagementObject m in hardDiskC)
+                {
+                    result += (m["VideoProcessor"].ToString().Replace("Family", "") + ToGB(Convert.ToInt64(m["AdapterRAM"].ToString()), 1024.0).ToString());
+                    break;
+                }
             }
             catch
             {
-                return null;
+
             }
+            return result;
         }
-        /// <summary>
+
+
+
+
         /// 检测主板信息
-        /// </summary>
-        public ManagementObjectCollection MainBoardInfo()
+        public string GetMainBoardInfo()
         {
+            var result = "";
             try
             {
                 ManagementClass mc = new ManagementClass(WMIPath.Win32_BaseBoard.ToString());
                 ManagementObjectCollection moc = mc.GetInstances();
-                return moc;
+                SelectQuery Query = new SelectQuery("SELECT * FROM Win32_BaseBoard");//查询主板
+                ManagementObjectSearcher driveID = new ManagementObjectSearcher(Query);//创建WMI查询对象
+                //获取查询结果
+                ManagementObjectCollection.ManagementObjectEnumerator data = driveID.Get().GetEnumerator();
+                data.MoveNext();//循环读取
+                ManagementBaseObject board = data.Current;//获取当前主板
+
+                foreach (var item in moc)
+                {
+
+                    result += item.Properties["Manufacturer"].Value.ToString() + " " + item.Properties["SerialNumber"].Value.ToString() + " " + item.Properties["Product"].Value + "\r\n";
+
+
+                }
+
+
+                return result;
             }
             catch
             {
                 return null;
             }
+
         }
 
         /// <summary>
         /// 检测网络连接信息
         /// </summary>
-        public ManagementObjectCollection NetworkInfo()
+        public string GetNetworkInfo()
         {
+            var result = "";
             try
             {
                 ManagementClass mc = new ManagementClass(WMIPath.Win32_NetworkAdapterConfiguration.ToString());
                 ManagementObjectCollection moc = mc.GetInstances();
-                return moc;
+                foreach (var item in moc)
+                {
+
+                    result += "IP地址：" + item.Properties["IPAddress"].Value + "\r\n";
+                    //result += "默认网关：" + item.Properties[DefaultIPGateway"].Value + "\r\n";
+                }
+                return result;
             }
             catch
             {
                 return null;
             }
 
+        }
+        /*/// <summary></summary>   
+        /// 显示本机各网卡的详细信息   
+        /// <summary></summary>   
+        public string[] GetNetworkInterfaceMessage()
+        {
+            //var result = "";
+            string[] result=new string[2];
+            NetworkInterface[] fNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in fNetworkInterfaces)
+            {   
+                #region " 网卡类型 "
+                string fCardType = "未知网卡";
+                string fRegistryKey = "SYSTEM\\CurrentControlSet\\Control\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\" + adapter.Id + "\\Connection";
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey(fRegistryKey, false);
+                if (rk != null)
+                {
+                    // 区分 PnpInstanceID    
+                    // 如果前面有 PCI 就是本机的真实网卡   
+                    // MediaSubType 为 01 则是常见网卡，02为无线网卡。   
+                    string fPnpInstanceID = rk.GetValue("PnpInstanceID", "").ToString();
+                    int fMediaSubType = Convert.ToInt32(rk.GetValue("MediaSubType", 0));
+                    if (fPnpInstanceID.Length > 3 &&
+                        fPnpInstanceID.Substring(0, 3) == "PCI")
+                        fCardType = "物理网卡";
+                    else if (fMediaSubType == 1)
+                        fCardType = "虚拟网卡";
+                    else if (fMediaSubType == 2)
+                        fCardType = "无线网卡";
+                }
+                #endregion
+                #region " 网卡信息 "
+
+                IPInterfaceProperties fIPInterfaceProperties = adapter.GetIPProperties();
+                UnicastIPAddressInformationCollection UnicastIPAddressInformationCollection = fIPInterfaceProperties.UnicastAddresses;
+                foreach (UnicastIPAddressInformation UnicastIPAddressInformation in UnicastIPAddressInformationCollection)
+                {
+                    if (UnicastIPAddressInformation.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)//AddressFamily.InterNetwork)
+                       result[1]=UnicastIPAddressInformation.Address.ToString();// Ip 地址   
+                }
+                result[0]= adapter.Name;
+                    
+                #endregion
+                   
+            }
+            return result;
+        }   */
+        /// <summary>
+        /// 获取授信站点列表
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetTrustyStations()
+        {
+            string[] subkeyNames;
+            RegistryKey hkml = Registry.CurrentUser;
+            RegistryKey software = hkml.OpenSubKey("SOFTWARE", true);
+            RegistryKey aimdir = software.OpenSubKey(@"Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains", true);
+            subkeyNames = aimdir.GetSubKeyNames();
+            //判断有没有域名前缀例如“www”
+            for (int i = 0; i < subkeyNames.Length; i++)
+            {
+                RegistryKey temp = aimdir.OpenSubKey(subkeyNames[i], false);
+                if (temp.GetSubKeyNames().Length == 1)
+                {
+                    subkeyNames[i] = temp.GetSubKeyNames()[0] + "." + subkeyNames[i];
+                }
+
+            }
+            hkml.Close();
+            return subkeyNames;
+        }
+
+        public string ToGB(double size, double mod)
+        {
+            String[] units = new String[] { "B", "KB", "MB", "GB", "TB", "PB" };
+            int i = 0;
+            while (size >= mod)
+            {
+                size /= mod;
+                i++;
+            }
+            return Math.Round(size) + units[i];
         }
      
     }
